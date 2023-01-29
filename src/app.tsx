347ddc3788ba3be38ba58a1d1313c3e379456f28
Matheus <matheusalmeida016@gmail.com>
2023-01-29 12:29:38 -0400

    Add setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ReactComponent as Logo } from './logo.svg'
+import { ReactComponent as Logo } from './assets/logo.svg'
 import './app.css'
 
 export function App () {
